89527f8a440fc811abfc67b3c1a4ae3bbbf3473a
Yurii Veselov <xoxorrrr@gmail.com>
2016-09-22 00:17:34 +0300

    Create slider.js

---
+++ js/slider.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    $(document).sliderUi({
+        slideHeight:'auto',
+        slideWidth:'auto',
+        slideCont:'#slider',
+        slideAuto:false,
+        SlideSpeed : 700,
+        TimeOut :3000,
+        NeedLinks :true,
+        slideControl:true,
+
+    });
+
+
+});
